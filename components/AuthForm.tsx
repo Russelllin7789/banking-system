8b5748bc07c212b588dc9a6540515cab4a57aafb
Russell Lin <superbr7789@gmail.com>
2024-07-09 21:14:36 +0800

    add shadcn form component and zod for validation

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,9 +2,33 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const AuthForm = ({ type }: AuthFormProps) => {
   const [user, setUser] = useState(null);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
     <section className="auth-form">
