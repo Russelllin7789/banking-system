634b8025ba2ce3624f87f416782bd9f3f96fa398
Russell Lin <superbr7789@gmail.com>
2024-07-09 21:10:51 +0800

    add form title for auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 
 const AuthForm = ({ type }: AuthFormProps) => {
+  const [user, setUser] = useState(null);
+
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8">
@@ -20,7 +23,26 @@ const AuthForm = ({ type }: AuthFormProps) => {
             Horizon
           </h1>
         </Link>
+
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          <p className="text-16 font-normal text-gray-600">
+            {user
+              ? "Link your account to get started"
+              : "Please enter your details"}
+          </p>
+        </div>
       </header>
+
+      {user ? (
+        <div className="flex flex-col gap-4"></div>
+      ) : (
+        <>
+          <div></div>
+        </>
+      )}
     </section>
   );
 };
