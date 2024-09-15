e73d0fc4f652e9bd69389eff81a0348863b80339
Russell Lin <superbr7789@gmail.com>
2024-09-15 22:00:09 +0800

    add bank tab item component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
@@ -32,7 +33,12 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT
+        <RecentTransactions
+          accounts={accountsData}
+          appwriteItemId={appwriteItemId}
+          transactions={account?.transactions}
+          page={Number(page)}
+        />
       </div>
       <RightSidebar
         user={loggedIn}
