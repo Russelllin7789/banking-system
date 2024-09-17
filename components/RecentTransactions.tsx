8313245aca6666e84de9fdc9547852a52e7da9b9
Russell Lin <superbr7789@gmail.com>
2024-09-17 16:44:38 +0800

    add bank-info component

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BankTabItem } from "./BankTabItem";
 
 const RecentTransactions = ({
   accounts,
@@ -22,9 +23,23 @@ const RecentTransactions = ({
 
       <Tabs defaultValue={appwriteItemId} className="w-full">
         <TabsList className="recent-transactions-tablist">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+          {accounts.map((account: Account) => (
+            <TabsTrigger key={account.id} value={account.appwriteItemId}>
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
+              />
+            </TabsTrigger>
+          ))}
         </TabsList>
+        {accounts.map((account: Account) => (
+          <TabsContent
+            key={account.id}
+            value={account.appwriteItemId}
+            className="space-y-4"
+          ></TabsContent>
+        ))}
       </Tabs>
     </section>
   );
