fdccb3c41cdcbadde0f3d36abb6c5804a9af806d
Russell Lin <superbr7789@gmail.com>
2024-09-19 09:08:13 +0800

    add getBankByAccountId server action and edit payment-transfer page

--- app/(root)/payment-tranfer/page.tsx
+++ app/(root)/payment-tranfer/page.tsx
@@ -1,7 +1,16 @@
 import HeaderBox from "@/components/HeaderBox";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const PaymentTransfer = () => {
+const PaymentTransfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn?.$id as string });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+
   return (
     <section className="payment-transfer">
       <HeaderBox
